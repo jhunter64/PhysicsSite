bf86c7adf790bab01a5922957cba9ea949bd8568
Will Hunter <jhunter64@gatech.edu>
2018-05-15 11:06:12 -0400

    Two columns and work calculation

--- testJS.js
+++ testJS.js
@@ -1,10 +1,18 @@
 function momentumCalculate() {
-		var mass = document.getElementById("mass").value 
-		var velocity = document.getElementById("velocity").value
-		var momentum = mass * velocity
-		document.getElementById("momentum").innerHTML += momentum
+	var mass = document.getElementById("mass").value 
+	var velocity = document.getElementById("velocity").value
+	var momentum = mass * velocity
+	document.getElementById("momentum").innerHTML = "Momentum: " + momentum
 }
+
 function changeColor() {
-		var p = document.getElementById("momentum");
-		p.style.color = "red"
+	var p = document.getElementById("momentum");
+	p.style.color = "red"
+}
+
+function workCalculate() {
+	var force = document.getElementById("force").value
+	var distance = document.getElementById("distance").value
+	var work = force * distance
+	document.getElementById("work").innerHTML = "Work: " + work
 }
