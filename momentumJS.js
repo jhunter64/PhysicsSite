ec251d4c7a10a83b680afdedb3e8b1e030889807
Will Hunter <jhunter64@gatech.edu>
2018-06-24 21:18:08 -0400

    Time Step magnitude

--- momentumJS.js
+++ momentumJS.js
@@ -6,7 +6,7 @@ function momentumCalculate() {
 	var isVector = document.getElementById("inputVector").checked
 	if (momentum == "") {
 		if (isMagnitude) {
-			momentum = mass * velocity	
+			momentum = mass * velocity;
 		}
 		else if (isVector) {
 			var v1 = parseVector(velocity);
@@ -17,7 +17,7 @@ function momentumCalculate() {
 	}
 	if (mass == "") {
 		if (isMagnitude) {
-			mass = momentum / velocity	
+			mass = momentum / velocity;
 		}
 		else if (isVector) {
 			var v1 = parseVector(velocity);
@@ -28,57 +28,61 @@ function momentumCalculate() {
 				mass = 0;
 			}
 		}
-		document.getElementById("mass").value = mass
+		document.getElementById("mass").value = mass;
 	}
 	if (velocity == "") {
 		if (isMagnitude) {
-			velocity = momentum / mass
+			velocity = momentum / mass;
 		}
 		if (isVector) {
 			var m1 = parseVector(momentum);
 			var v1 = multiplyVector(m1, 1.0 / mass);
 			velocity = toStringVector(v1);
 		}
-		document.getElementById("velocity").value = velocity
+		document.getElementById("velocity").value = velocity;
 	}
 }
 
 function collisionCalculate() {
-	var m1 = document.getElementById("m1").value
-	var m2 = document.getElementById("m2").value
-	var vi1 = document.getElementById("vi1").value
-	var vi2 = document.getElementById("vi2").value
-	var vf1 = document.getElementById("vf1").value
-	var vf2 = document.getElementById("vf2").value
-	var isMagnitude = document.getElementById("inputMagnitude").checked
-	var isVector = document.getElementById("inputVector").checked
+	var m1 = document.getElementById("col_m1").value;
+	var m2 = document.getElementById("col_m2").value;
+	var vi1 = document.getElementById("col_vi1").value;
+	var vi2 = document.getElementById("col_vi2").value;
+	var vf1 = document.getElementById("col_vf1").value;
+	var vf2 = document.getElementById("col_vf2").value;
+	var isMagnitude = document.getElementById("inputMagnitude").checked;
+	var isVector = document.getElementById("inputVector").checked;
 	if (vf1 == "") {
 		var pi = m1 * vi1 + m2 * vi2;
 		var pf1 = pi - m2 * vf2;
 		vf1 = pf1 / m1;
-		document.getElementById("vf1").value = vf1;
+		document.getElementById("col_vf1").value = vf1;
 	}
 	if (vf2 == "") {
 		var pi = m1 * vi1 + m2 * vi2;
 		var pf2 = pi - m1 * vf1;
 		vf2 = pf2 / m2;
-		document.getElementById("vf2").value = vf2;
+		document.getElementById("col_vf2").value = vf2;
 	}
 }
 
 function timeStepCalculate() {
-	var position;
-	var force;
-	var velocity;
-	var mass;
-	var timeInterval;
-}
+	var mass = document.getElementById("ts_m").value;
+	var velocity = document.getElementById("ts_v").value;
+	var force = document.getElementById("ts_f").value;
+	var position = document.getElementById("ts_r").value;
+	var timeInterval = document.getElementById("ts_t").value;
 
-function workCalculate() {
-	var force = document.getElementById("force").value
-	var distance = document.getElementById("distance").value
-	var work = force * distance
-	document.getElementById("work").innerHTML = "Work: " + work
+	var pi = mass * velocity;
+	var pf = pi + force * timeInterval;
+	var vf = pf / mass;
+	var ri = position / 1;
+	var rf = ri + vf * timeInterval;
+	document.getElementById("ts_m").value = mass;
+	document.getElementById("ts_v").value = vf;
+	document.getElementById("ts_f").value = force;
+	document.getElementById("ts_r").value = rf;
+	document.getElementById("ts_t").value = timeInterval;
 }
 
 function changeType(curr, newType) {
@@ -86,16 +90,24 @@ function changeType(curr, newType) {
 }
 
 function resetMomentum() {
-	document.getElementById("mass").value = ""
-	document.getElementById("velocity").value = ""
-	document.getElementById("momentum").value = ""
+	document.getElementById("mass").value = "";
+	document.getElementById("velocity").value = "";
+	document.getElementById("momentum").value = "";
 }
 
 function resetCollision() {
-	document.getElementById("m1").value = ""
-	document.getElementById("m2").value = ""
-	document.getElementById("vi1").value = ""
-	document.getElementById("vi2").value = ""
-	document.getElementById("vf1").value = ""
-	document.getElementById("vf2").value = ""
+	document.getElementById("col_m1").value = "";
+	document.getElementById("col_m2").value = "";
+	document.getElementById("col_vi1").value = "";
+	document.getElementById("col_vi2").value = "";
+	document.getElementById("col_vf1").value = "";
+	document.getElementById("col_vf2").value = "";
+}
+
+function resetTimeStep() {
+	document.getElementById("ts_m").value = "";
+	document.getElementById("ts_v").value = "";
+	document.getElementById("ts_f").value = "";
+	document.getElementById("ts_r").value = "";
+	document.getElementById("ts_t").value = "";
 }
